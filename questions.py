91f72272ccb9342315b85471492ac3fd385de7b2
Anish Gurjar <50738829+anishgurjar@users.noreply.github.com>
2022-02-22 16:33:00 -0500

    Added Question, Options and Answers Object

---
+++ questions.py
@@ -0,0 +1,82 @@
+questionlist=[
+    [
+       { "question":" The Ozone layer prohibits",
+         "options":["A)Visible light","B)Infrared radiation","C)X-rays and gamma rays ","D)Ultraviolet radiation"],
+         "answer":3 },
+       { "question":" The Ozone layer prohibits 2",
+         "options":["A)Visible light","B)Infrared radiation","C)X-rays and gamma rays ","D)Ultraviolet radiation"],
+         "answer":3 }
+       ],
+    [
+       { "question":"Euclid was",
+         "options":["A) Greek mathematician","B)Contributor to the use of deductive principles of logic as the basis of geometry","C)Propounded the geometrical theorems ","D)All of the above"],
+         "answer":3 },
+       { "question":"Euclid was 2",
+         "options":["A) Greek mathematician","B)Contributor to the use of deductive principles of logic as the basis of geometry","C)Propounded the geometrical theorems ","D)All of the above"],
+         "answer":3 }
+       ],
+    [
+       { "question":"Which of the following personalities is known as the father of economics?",
+         "options":["A)J.M. Keynes","B)Adam Smith","C)Abraham Maslow ","D)J.K. Galbraith"],
+         "answer":1 },
+       { "question":"Which of the following personalities is known as the father of economics? 2",
+         "options":["A)J.M. Keynes","B)Adam Smith","C)Abraham Maslow ","D)J.K. Galbraith"],
+         "answer":1 }
+       ],
+    [
+       { "question":"Which of the following is the capital of Ethiopia?",
+         "options":["A)Abuja","B) Dar es Salaam","C) Addis Ababa ","D) Harare"],
+         "answer":2 },
+       { "question":"Which of the following is the capital of Ethiopia? 2",
+         "options":["A)Abuja","B) Dar es Salaam","C) Addis Ababa ","D) Harare"],
+         "answer":2 }
+       ],
+    [
+       { "question":"When did Mother Teresa win the Nobel Peace Prize?",
+         "options":["A)1975","B)1979","C)1981 ","D) 1982"],
+         "answer":1 },
+       { "question":"When did Mother Teresa win the Nobel Peace Prize? 2",
+         "options":["A)1975","B)1979","C)1981 ","D) 1982"],
+         "answer":1 }
+       ],
+    [
+       { "question":"Who invented the Ballpoint pen?",
+         "options":["A)Biro Brothers","B)Waterman Brothers","C)Write Brothers ","D)Bicc Brothers"],
+         "answer":0 },
+       { "question":"Who invented the Ballpoint pen? 2",
+         "options":["A)Biro Brothers","B)Waterman Brothers","C)Write Brothers ","D)Bicc Brothers"],
+         "answer":0 }
+       ],
+    [
+       { "question":"In which decade was the first solid state integrated circuit demonstrated?",
+         "options":["A)1950s","B)1950s","C)1970s ","D)1980s"],
+         "answer":0 },
+       { "question":"In which decade was the first solid state integrated circuit demonstrated? 2",
+         "options":["A)1950s","B)1950s","C)1970s ","D)1980s"],
+         "answer":0 }
+       ],
+    [
+       { "question":"Which scientist discovered the radioactive element radium?",
+         "options":["A)Isaac Newton","B)Albert Einstein","C)Benjamin Franklin ","D)Marie Curie"],
+         "answer":3 },
+       { "question":"Which scientist discovered the radioactive element radium? 2",
+         "options":["A)Isaac Newton","B)Albert Einstein","C)Benjamin Franklin ","D)Marie Curie"],
+         "answer":3 }
+       ],
+    [
+       { "question":"What does NATO stand for?",
+         "options":["A)North Atlantic Treaty Organization","B)North America Treaty Organization","C)North Atlantic Trust Organization ","D) North Alliance Treaty Organization"],
+         "answer":0 },
+       { "question":"What does NATO stand for? 2",
+         "options":["A)North Atlantic Treaty Organization","B)North America Treaty Organization","C)North Atlantic Trust Organization ","D) North Alliance Treaty Organization"],
+         "answer":0 }
+       ],
+    [
+       { "question":"Which is the smallest country in the world?",
+         "options":["A)Vanuatu","B)Vatican City","C) Luxembourg ","D)Monaco"],
+         "answer":1 },
+       { "question":"Which is the smallest country in the world? 2",
+         "options":["A)Vanuatu","B)Vatican City","C) Luxembourg ","D)Monaco"],
+         "answer":1 }
+       ]
+]
